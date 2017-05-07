968ce2f9d534678c53f488c111fa067edf6020f7
eternalshadow7 <greentomboy@sbcglobal.net>
2017-05-07 15:51:15 -0700

    more conflict

--- game01/js/HowToPlay.js
+++ game01/js/HowToPlay.js
@@ -4,14 +4,14 @@ myGame.HowToPlay.prototype={
 	
 	create: function(){
 		// This will show the background for the How to Play Screen
-		this.game.add.sprite(0, 0, 'Instructions');
+		this.game.add.sprite(1, 1, 'Instructions');
 		
 		// This is for creating a button so the player can click on it to switch states back to the game screen.
 		button=this.game.add.button(30, 9,'BackArrow', actionOnClick);
 		button.scale.setTo(2, 2);
 	        
 		//This is an intentional conflict
-		button.scale.setTo(3,2);
+		button.scale.setTo(1,1);
 		
 	 // This is a click function so that what the button is clicked, the player will go to the Main Menu.
      function actionOnClick(){
